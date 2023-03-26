3d3ab60f40642ea24f2999f7669e88bf8475de08
李耀雲 <12682046+li-yaoyun@user.noreply.gitee.com>
2023-03-26 11:25:59 +0000

    update src/main/java/com/liyaoyun/Main.java.
    Signed-off-by: 李耀雲 <12682046+li-yaoyun@user.noreply.gitee.com>

--- src/main/java/com/liyaoyun/Main.java
+++ src/main/java/com/liyaoyun/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("nihao");
         System.out.println("shijie");
+        System.out.println("gitee");
     }
 }
